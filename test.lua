278807617e857187a1c93bad4a184e3eca66827b
Roy Moore <roy@moore.co.il>
2025-06-07 22:22:48 +0300

    Minimap icon

--- test.lua
+++ test.lua
@@ -64,3 +64,16 @@ myFrame:SetScript("OnDragStop", function(self)
     myFrameText:SetText(xpString) -- Update the text in the frame
 end)
 myFrame:Show()
+
+-- Create a mini map icon
+local myMiniMapIcon = CreateFrame("Button", "MyMiniMapIcon", Minimap)
+myMiniMapIcon:SetSize(16, 16)
+myMiniMapIcon:SetPoint("TOPRIGHT", Minimap, "TOPRIGHT", -5, -5)
+myMiniMapIcon:SetNormalTexture("Interface\\Icons\\INV_Misc_QuestionMark")
+myMiniMapIcon:SetScript("OnClick", function()
+    if myFrame:IsShown() then
+        myFrame:Hide()
+    else
+        myFrame:Show()
+    end
+end)
