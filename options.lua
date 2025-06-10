78b5b468fd7553540f199a67a9833276c13d1fa3
Roy Moore <roy@moore.co.il>
2025-06-10 22:32:42 +0300

    Refactor options

---
+++ options.lua
@@ -0,0 +1,60 @@
+-- Options to configure the behavior of the script
+function initOptions(addonRef)
+    name = addonRef.name
+    L = addonRef.L
+    frame = addonRef.frame
+
+    local options = {
+        type = "group",
+        name = addonName,
+        width = "normal",
+        args = {
+            header = {
+                type = "header",
+                order = 10,
+                name = L["Settings"],
+            },
+            showFrame = {
+                type = "toggle",
+                order = 20, 
+                name = L["Show Frame"],
+                desc = L["Toggle the visibility of the player progression frame."],
+                get = function() return addonRef.db.profile.showFrame end,
+                set = function(_, value)
+                    addonRef.db.profile.showFrame = value
+                    if value then
+                        frame:Show()
+                    else
+                        frame:Hide()
+                    end
+                end,
+            },
+            maxSamples = {
+                type = "range",
+                order = 30, 
+                name = L["Max XP Snapshots"], -- CHANGED text
+                desc = L["Set the maximum number of recent XP snapshots to store for rate calculation."], -- CHANGED text
+                min = 2, -- Need at least 2 for a rate
+                max = 20,
+                step = 1,
+                get = function(info)
+                    return addonRef.db.profile.maxSamples
+                end,
+                set = function(info, value)
+                    addonRef.db.profile.maxSamples = value
+                    -- Debug print to confirm change
+                    print(addonName .. ": Max XP Snapshots changed to " .. addonRef.db.profile.maxSamples)
+                    -- Prune snapshots if new maxSamples is less than current number of snapshots
+                    if addonRef.db.profile.xpSnapshots then
+                        while #addonRef.db.profile.xpSnapshots > addonRef.db.profile.maxSamples do
+                            table.remove(addonRef.db.profile.xpSnapshots, 1)
+                        end
+                    end
+                    addonRef:UpdateXP() 
+                end,
+            },
+        },
+    }
+
+    return options
+end
--- xpInfo.lua
+++ xpInfo.lua
@@ -42,60 +42,9 @@ function addon:OnInitialize()
     self:RegisterEvent("PLAYER_ENTERING_WORLD", "OnPlayerEnteringWorld")
     self:RegisterEvent("TIME_PLAYED_MSG", "OnTimePlayedMessage")
 
-    local addonRef = self
-
-    local options = {
-        type = "group",
-        name = addonName,
-        width = "normal",
-        args = {
-            header = {
-                type = "header",
-                order = 10,
-                name = L["Settings"],
-            },
-            showFrame = {
-                type = "toggle",
-                order = 20, 
-                name = L["Show Frame"],
-                desc = L["Toggle the visibility of the player progression frame."],
-                get = function() return addonRef.db.profile.showFrame end,
-                set = function(_, value)
-                    addonRef.db.profile.showFrame = value
-                    if value then
-                        frame:Show()
-                    else
-                        frame:Hide()
-                    end
-                end,
-            },
-            maxSamples = {
-                type = "range",
-                order = 30, 
-                name = L["Max XP Snapshots"], -- CHANGED text
-                desc = L["Set the maximum number of recent XP snapshots to store for rate calculation."], -- CHANGED text
-                min = 2, -- Need at least 2 for a rate
-                max = 20,
-                step = 1,
-                get = function(info)
-                    return addonRef.db.profile.maxSamples
-                end,
-                set = function(info, value)
-                    addonRef.db.profile.maxSamples = value
-                    -- Debug print to confirm change
-                    print(addonName .. ": Max XP Snapshots changed to " .. addonRef.db.profile.maxSamples)
-                    -- Prune snapshots if new maxSamples is less than current number of snapshots
-                    if addonRef.db.profile.xpSnapshots then
-                        while #addonRef.db.profile.xpSnapshots > addonRef.db.profile.maxSamples do
-                            table.remove(addonRef.db.profile.xpSnapshots, 1)
-                        end
-                    end
-                    addonRef:UpdateXP() 
-                end,
-            },
-        },
-    }
-
+    self.L = L -- Store the localization table in the addon for easy access
+    self.frame = frame -- Store the frame reference in the addon for easy access
+    options = initOptions(self) -- Initialize options with a reference to this addon
     LibStub("AceConfig-3.0"):RegisterOptionsTable(addonName, options)
     self:CreateFrame()
 end
